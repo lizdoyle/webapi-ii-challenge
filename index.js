b521bcfa42819de8870e2fa69a848e9db7466216
lizdoyle <lizdoyle1121@gmail.com>
2019-11-03 23:47:30 -0500

    1st get request, not tested

--- index.js
+++ index.js
@@ -10,11 +10,56 @@ server.get('/', (req, res) => {
 
 
 server.post('/api/posts', (req, res) => {
+    const postInfo = req.body;
 
-
-
+    if (!postInfo.title || !postInfo.contents) {
+        return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    else {
+        db.insert(postInfo)
+            .then(post => {
+                res.status(201).json(post);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    }
 
 })
+
+server.post('api/posts/:id/comments', (req, res) => {
+    const postInfo = req.body;
+    const id = req.params.id;
+
+    if(!id) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+    else if (!postInfo.text) {
+        return res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    }
+    else {
+        db.insert(postInfo)
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the comment to the database" })
+            })
+    }
+
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            return res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+
 // watch for connections on port 5000
 server.listen(5000, () =>
   console.log('\n===Server running on http://localhost:5000===\n')
